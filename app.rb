bbc96461666702292eec18a6a02b444b144f44a7
Pooja Pasawala <pooja@basicer.com>
2014-02-22 19:27:08 -0500

    Comment out routes in app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'bundler'
 Bundler.require
 require 'sinatra/json'
-require './lib/playlister'
-require './lib/parser'
+require './lib/playlister.rb'
+require './lib/parser.rb'
 
 module Playlist
 class PlayListerApp < Sinatra::Application
@@ -21,30 +21,30 @@ class PlayListerApp < Sinatra::Application
     @artists.to_json
   end
   
-  get '/artists/:artist' do
-    @artist = Artist.find_by_name(params[:artist])
-    @artist.to_json
-  end
+  # get '/artists/:artist' do
+  #   @artist = Artist.find_by_name(params[:artist])
+  #   @artist.to_json
+  # end
 
   get '/genres' do
     @genres = Genre.list
     @genres.to_json
   end
 
-  get '/genres/:genre' do
-    @genre = Genre.find_by_name(params[:genre])
-    @genre.to_json
-  end
+  # get '/genres/:genre' do
+  #   @genre = Genre.find_by_name(params[:genre])
+  #   @genre.to_json
+  # end
 
   get '/songs' do
     @songs = Song.list
     @songs.to_json
   end
 
-  get '/songs/:song' do
-    @song = Song.find_by_name(params[:song])
-    @song.to_json
-  end
+  # get '/songs/:song' do
+  #   @song = Song.find_by_name(params[:song])
+  #   @song.to_json
+  # end
 
 end
 end
