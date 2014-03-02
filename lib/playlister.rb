5356d24dbabb6d27184afd552a2948ad013719a9
Pooja Pasawala <pooja@basicer.com>
2014-03-02 01:57:10 -0500

    Delete unnecessary file

--- lib/playlister.rb
+++
@@ -1,3 +0,0 @@
-# class PlayLister < ActiveRecord::Model
-
-# end
--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// in angular controller, when u $http.get( <datasource>),
-// specify the route for the <datasource>
-// then you pass in 'data' into the anonymous  function
-//and set $scope = data
-
 /* Controllers */
 playListerApp.controller("PlayListerCtrl", ["$scope", "$http", function($scope, $http){
 
